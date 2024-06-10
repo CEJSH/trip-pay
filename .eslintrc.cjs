ef23bb6a5670494b3ef07e768ba566d763cd2169
CEJSH <eun2763@naver.com>
2024-06-10 16:44:32 +0900

    eslint-unused-vars-off

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ module.exports = {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "warn",
-    "no-unused-vars": "warn",
+    "no-unused-vars": "off",
     "no-useless-catch": "off",
     "no-nested-ternary": "off",
     "@typescript-eslint/no-var-requires": "off",
